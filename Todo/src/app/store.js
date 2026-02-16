9f5aa0820cc462f228bdd17e03759c16b281b913
M-naveel <mnaveel051@gmail.com>
2026-02-16 17:23:30 +0500

    Local Storage
    deployed the local storage functionality

--- Todo/src/app/store.js
+++ Todo/src/app/store.js
@@ -1,8 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
-import todoReducer from "./features/todo/todoSlice"
+import todoReducer from "./features/todo/todoSlice";
 
 
 export const store = configureStore({
 
-    reducer:  todoReducer 
+    reducer: todoReducer 
+})
+
+store.subscribe(()=>{
+    const state = store.getState();
+    localStorage.setItem("todos", JSON.stringify(state.todos))
 })
