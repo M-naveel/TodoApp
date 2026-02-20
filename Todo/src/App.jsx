f8a9871774e142bae04be7618a97e290c165af22
M-naveel <mnaveel051@gmail.com>
2026-02-20 15:18:24 +0500

    HeaderAndFooter
    Successfully  Added the Navbar and Footer in the App and  added the responsiveness for the mobile screen using the tailwind Css.

--- Todo/src/App.jsx
+++ Todo/src/App.jsx
@@ -2,15 +2,21 @@ import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './Component/Navbar'
+import Footer from './Component/Footer'
 import AddTodo from './Component/AddTodo'
 import Todos from './Component/Todos'
 function App() {
 
 
   return (
-    <>
+    <>    
+    
+          <Navbar />
           <AddTodo />
           <Todos />
+          <Footer />
+    
     </>
   )
 }
---
+++ Todo/src/Component/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <>
+      <header class="bg-zinc-900  text-white rounded-xl  z-50">
+  <div class="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center ">
+    
+
+    <h1 class="text-xl font-bold">Todo App</h1>
+
+  </div>
+</header>
+    </>
+  )
+}
+
+export default Navbar
